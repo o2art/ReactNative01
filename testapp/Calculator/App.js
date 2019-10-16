a49b74eb4f774881c8b4d7f6940ded1bd55497b3
Tymek Lenart <tymek@lenart.org.pl>
2019-10-16 22:09:52 +0200

    Add calc app

--- testapp/Calculator/App.js
+++ testapp/Calculator/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  SafeAreaView,
-  Platform
-} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-import Row from "./Row";
 import Button from "./Button";
-import Calculator, { initialState } from "./Calculator";
-
-const STATUSBAR_HEIGHT = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#202020",
-    justifyContent: "flex-end"
+    flex: 3
   },
   value: {
+    flex: 1,
+    backgroundColor: "#097973",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  valuetxt: {
     color: "#fff",
-    fontSize: 40,
-    textAlign: "right",
-    marginRight: 20,
-    marginBottom: 10
+    fontSize: 32
+  },
+  result: {
+    flex: 1,
+    backgroundColor: "#097973",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  resulttxt: {
+    color: "#fff",
+    fontSize: 24
+  },
+  button: {
+    flexDirection: "row",
+    flex: 1
+  },
+  buttons: {
+    flex: 3,
+    flexDirection: "row",
+    backgroundColor: "#3c3c3c"
   }
 });
 
@@ -33,20 +42,105 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      exp: 0,
-      out: 0
+      currentValue: "",
+      previousValue: "",
+      result: ""
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  handleClick(val) {
+    if (val != "C" && val != "=") {
+      this.setState({
+        currentValue: this.state.currentValue + val,
+        previousValue: val,
+        result: ""
+      });
+    } else if (val == "=") {
+      let option = "";
+      if (
+        isNaN(this.state.currentValue.substr(-1)) ||
+        isNaN(this.state.currentValue.substr(0, 1)) ||
+        this.state.currentValue.includes("00.")
+      ) {
+        option = "błąd";
+      } else {
+        option = parseFloat(eval(this.state.currentValue)).toFixed(1);
+      }
+      this.setState({
+        result: option
+      });
+    } else {
+      this.setState({
+        currentValue: this.state.currentValue.slice(
+          0,
+          this.state.currentValue.length - 1
+        ),
+        previousValue: this.state.currentValue.slice(
+          this.state.currentValue.length - 2,
+          this.state.currentValue.length - 1
+        ),
+        result: ""
+      });
+    }
+  }
+
+  clear() {
+    this.setState({
+      currentValue: "",
+      result: ""
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <SafeAreaView>
-          <Row>
-            <Button text="1" onPress={this._onPress}></Button>
-          </Row>
-        </SafeAreaView>
+        <View style={styles.value}>
+          <Text style={styles.valuetxt}> {this.state.currentValue} </Text>
+        </View>
+        <View style={styles.result}>
+          <Text style={styles.resulttxt}> {this.state.result} </Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={{ flexDirection: "column", flex: 5 }}>
+            <View style={styles.button}>
+              <Button key={1} value={1} handleClick={this.handleClick} />
+              <Button key={2} value={2} handleClick={this.handleClick} />
+              <Button key={3} value={3} handleClick={this.handleClick} />
+            </View>
+            <View style={styles.button}>
+              <Button key={4} value={4} handleClick={this.handleClick} />
+              <Button key={5} value={5} handleClick={this.handleClick} />
+              <Button key={6} value={6} handleClick={this.handleClick} />
+            </View>
+            <View style={styles.button}>
+              <Button key={7} value={7} handleClick={this.handleClick} />
+              <Button key={8} value={8} handleClick={this.handleClick} />
+              <Button key={9} value={9} handleClick={this.handleClick} />
+            </View>
+            <View style={styles.button}>
+              <Button key={"."} value={"."} handleClick={this.handleClick} />
+              <Button key={0} value={0} handleClick={this.handleClick} />
+              <Button key={"="} value={"="} handleClick={this.handleClick} />
+            </View>
+          </View>
+          <View style={{ flexDirection: "column", flex: 2 }}>
+            <View style={{ flexDirection: "column", flex: 1 }}>
+              <Button
+                key={"C"}
+                value={"C"}
+                handleClick={this.handleClick}
+                clear={this.clear}
+              />
+              <Button key={"/"} value={"/"} handleClick={this.handleClick} />
+              <Button key={"*"} value={"*"} handleClick={this.handleClick} />
+              <Button key={"-"} value={"-"} handleClick={this.handleClick} />
+              <Button key={"+"} value={"+"} handleClick={this.handleClick} />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
--- testapp/Calculator/Button.js
+++ testapp/Calculator/Button.js
@@ -1,19 +1,67 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#fff",
+    fontSize: 25
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#3c3c3c",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "red",
+    borderStyle: "dotted",
+    borderWidth: 2,
+    borderRadius: 2,
+    margin: 2
+  },
+  special: {
+    backgroundColor: "#575757"
+  }
+});
 
 class Button extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
-    this.state = {};
+    this.state = {
+      value: this.props.value,
+      click: this.props.handleClick,
+      clear: this.props.clear
+    };
   }
 
   render() {
-    return (
-      <View>
-        <Text> Button </Text>
-      </View>
-    );
+    buttonStyles = [styles.button];
+    if (this.props.value != "C") {
+      if (
+        this.props.value == "/" ||
+        this.props.value == "*" ||
+        this.props.value == "-" ||
+        this.props.value == "+"
+      )
+        buttonStyles.push(styles.special);
+      return (
+        <TouchableOpacity
+          style={buttonStyles}
+          onPress={() => this.state.click(this.state.value)}
+        >
+          <Text style={styles.text}>{this.state.value}</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      buttonStyles.push(styles.special);
+      return (
+        <TouchableOpacity
+          style={buttonStyles}
+          onPress={() => this.state.click(this.state.value)}
+          onLongPress={this.state.clear}
+        >
+          <Text style={styles.text}>{this.state.value}</Text>
+        </TouchableOpacity>
+      );
+    }
   }
 }
 
